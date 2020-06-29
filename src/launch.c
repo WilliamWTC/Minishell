748aeebe900c1971a6fa7ceb6cbc780acb7b9794
William Mabitsela <williammemor@gmail.com>
2020-06-29 19:06:14 +0200

    Update launch.c

--- src/launch.c
+++ src/launch.c
@@ -8,7 +8,7 @@ int     ft_launch(char **args)
     pid = fork();
     if(pid == 0)
     {
-        if(execve(args[0] = "/bin/sh", args, NULL) == -1)
+        if(execve(args[0] = "/bin/tcsh", args, NULL) == -1)
         {
             ft_putstr("minishell: command not found: ");
             ft_putendl(args[0]);
