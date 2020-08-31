74d3bc562c8305754ba9614eeb94a1dc7f372cb8
mnchabeleng <35485956+mnchabeleng@users.noreply.github.com>
2020-08-31 21:26:23 +0200

    Delete valgrind.sh

--- valgrind.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Check form memory leaks
-make re
-clear
-valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-fds=yes --track-origins=yes --show-reachable=yes --verbose --gen-suppressions=all --log-file=valgrind-out.txt ./minishell
